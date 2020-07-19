3c9300da563c5a70995b9d2991b0242d0cc17252
guanliping <2289029828@qq.com>
2020-07-19 15:17:00 +0800

    study

--- geek-time_frontend/src/pages/study/Study.jsx
+++ geek-time_frontend/src/pages/study/Study.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
-import './Study.css';
+import './Study.styl';
 
 function Study(props) {
     return (
@@ -9,22 +9,23 @@ function Study(props) {
             {/* 头部 */}
             <div className="study-header">
                 <div className="study-title">我的学习</div>
-                <span class="iconfont icon-shijian"></span>
+                <span class="iconfont">&#xe632;</span>
             </div>
-            
+
             {/* 中间 */}
             <div className="study-body">
-                <div className="study-time">
-                    <span className="study-time-long">
-                        <span>本周时长</span>
-                        <span>0</span>
-                        <span>小时</span>
-                        <span>2</span>
-                        <span>分钟</span>
-                    </span>
-                </div>
                 <div className="study-items">
-                    <div className="study-item">
+                    <div className="study-time">
+                        <p className="study-time-long">
+                            <span>本周时长</span>
+                            <span className='yellow-num'>0</span>
+                            <span>小时</span>
+                            <span className='yellow-num'>2</span>
+                            <span>分钟</span>
+                        </p>
+                    </div>
+                    
+                    {/* <div className="study-item">
                         <p className="study-item-date">
                             <span>0</span>
                         </p>
@@ -47,14 +48,14 @@ function Study(props) {
                             <span>0</span>
                         </p>
                         <p className="study-item-name">下载</p>
-                    </div>
+                    </div> */}
                 </div>
 
                 <div className="study-order">
                     <p className="study-tips">暂无学习内容</p>
                     <button className="study-btn">订阅</button>
                 </div>
-            </div>
+             </div>
             {/* 底部 */}
             <div className="study-footer">
 
