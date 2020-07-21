44101bad0ff5f9dcaf550d7e362834b078b1c4f4
guanliping <2289029828@qq.com>
2020-07-21 09:49:55 +0800

    完成study页面

--- geek-time_frontend/src/pages/my/My.jsx
+++ geek-time_frontend/src/pages/my/My.jsx
@@ -5,8 +5,34 @@ import './My.css';
 
 function My(props) {
     return (
-        <div className="My">
-            My
+        <div className="my">
+           <div className="my-header">
+               <div className="my-info">
+                   <span className="iconfont">&#xe60b;</span>
+                   <span className="my-info__badge">
+                      11
+                   </span>
+               </div>
+           </div>
+           <div className="my-body">
+               <div className="my-body__infoms">
+                   <div className="my-body__infoms_avatar">
+                      <img src="https://static001.geekbang.org/account/avatar/00/1e/c8/cd/723dd9f9.jpg?x-oss-process=image/resize,m_fill,h_142,w_142" alt=""/>
+                   </div>
+                   <div className="my-body__infoms_mess">
+                        <h3>南风草木香🍀</h3>
+                        <div className="my-body__infoms_mess_phone">
+                            <p>157****5643</p>
+                            <a href="#">个人主页
+                                <span className="iconfont">&#xe534;</span>
+                            </a>
+                        </div>
+                   </div>
+               </div>
+               <div className="my-body__imgs"></div>
+               <div className="my-body__tabbars"></div>
+           </div>
+           <div className="my-footer"></div>
         </div>
     )
 }
