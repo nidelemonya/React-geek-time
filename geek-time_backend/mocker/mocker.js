c1ded590848515b889653771e2d32b251e7086cc
Bruce-shy <zhouzihan341308@163.com>
2020-07-27 22:57:07 +0800

    下拉刷新 上拉加载完成

--- geek-time_backend/mocker/mocker.js
+++ geek-time_backend/mocker/mocker.js
@@ -1,10 +1,10 @@
-const infos = require('./data/infos3.json');
+const infos = require('./data/infos.json');
 module.exports = {
     'GET /data/all': require('./data/all.json'),
     'GET /data/infos': (req, res) => {
         // 获取传进来的 参数offset
         const { offset = 0 } = req.query;
-        console.log(offset, req.query, Number(offset) , Number(offset)  + 10)
+        // console.log(offset, req.query, Number(offset) , Number(offset)  + 10)
         const data = infos.slice(Number(offset) , Number(offset)  + 10);
         res.json({
             "infos":data});
