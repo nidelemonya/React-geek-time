d3c611ae0611b38d54fabba7c051853a1a97f332
guanliping <2289029828@qq.com>
2020-07-31 09:45:56 +0800

    update payment页面

--- geek-time_frontend/src/pages/payment/Payment.jsx
+++ geek-time_frontend/src/pages/payment/Payment.jsx
@@ -8,54 +8,57 @@ function Pay(props) {
         <Container>
             <div className="pay">
                 <div className="pay-header">
-                    <span className="iconfont">&#xe571;</span>
-                    <h3 className="title">结算台</h3>
+                    <div className="pay-icon">
+                        <span className="iconfont">&#xe571;</span>
+                    </div>
+                    <div className="pay-title">
+                        <span>结算台</span>
+                    </div>
                 </div>
                 <div className="pay-container">
-                    {/* <div className="pay-info">
-                    <div className="pay-list">
-                        <img src="https://static001.geekbang.org/resource/image/99/b3/99e02ba6abf8e09dyy6d1d7951489ab3.jpg?x-oss-process=image/resize,m_fill,h_160,w_160" alt="cover" />
-                        <span>如何看懂一幅画</span>
-                        <span className="item-subtitle">22讲 | 678人已学习</span>
-                        <span className="item-price">
-                            <div className="price iconfont">¥ 19.90</div>
-                            <span className="item-count">x1</span>
-                        </span>
-                    </div>
-                </div>
-                <div className="checkout-panel">
-                    <div className="checkout-price">
-                        <span>总计:</span>
-                        <span className="price">
-                            <span className="iconfont">¥</span>
-                            19.90
-                        </span>
-                    </div>
-                </div>
-                <div className="pay-method">
-                    <h3 className="pay-method-h3">当前支付方式</h3>
-                    <div className="method-list-item">
-                        <span>
-                            <span className="iconfont"></span>
-                            <div className="pay-method-text">
-                                <em>微信支付</em>
-                            </div>
-                        </span>
-                        <div className="checkbox">
-                            <span className="iconfont checked"></span>
+                    <div className="pay-info">
+                        <div className="pay-list">
+                            <img src="https://static001.geekbang.org/resource/image/d3/3a/d3a96afa07fbbc5219ffe5e2ccaade3a.jpg?x-oss-process=image/resize,m_fill,h_216,w_164"/>
+                            <div className="pay-list-right">
+                                <p className="title">如何看懂一幅画</p>
+                                <p className="item-subtitle">22讲 | 678人已学习</p>
+                                <p className="item-price">
+                                    <span className="price">¥ 19.90</span>
+                                    <span className="item-count">x1</span>
+                                </p>
+                            </div> 
                         </div>
                     </div>
-                    <div className="choose-other-pay">
-                        <div className="choose-label">选择其他支付方式</div>
-                        <span className="iconfont"></span>
+                    <div className="checkout-panel">
+                        <div className="checkout-price">
+                            <span>总计:</span>
+                            <span className="price">¥19.90</span>
+                        </div>
                     </div>
-                    <p className="coupon-rules">· 本商品为虚拟内容服务，购买后不支持退换、转让，请斟酌确认。</p>
-                </div>
-                <p className="current-user">
-                    <span>当前登录账号：157****2750
+                    <div className="pay-method">
+                        <h3 className="pay-method-h3">当前支付方式</h3>
+                        <div className="method-list-item">
+                            <div>
+                                <span className="iconfont">&#xe614;</span>
+                                <span className="pay-method-text">
+                                    <em>微信支付</em>
+                                </span>
+                            </div>
+                            <div className="checkbox">
+                                <span className="iconfont checked">&#xe6ab;</span>
+                            </div>
+                        </div>
+                        {/* <div className="choose-other-pay">
+                            <div className="choose-label">选择其他支付方式</div>
+                            <span className="iconfont">&#xe534;</span>
+                        </div>
+                        <p className="coupon-rules">· 本商品为虚拟内容服务，购买后不支持退换、转让，请斟酌确认。</p> */}
+                    </div>
+                    {/* <p className="current-user">
+                        <span>当前登录账号：157****2750
                         <a className="orange">切换账号</a>
-                    </span>
-                </p> */}
+                        </span>
+                    </p>  */}
                     <div className="bottom">
                         <div className="bottom-name">¥19.90 确认支付</div>
                     </div>
