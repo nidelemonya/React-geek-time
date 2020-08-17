a345d0873cf52c86fcc54c9e49d2e86ff7791dbb
“Bruce-shy <zhouzihan341308@163.com>
2020-08-17 09:26:49 +0800

    项目上线

--- geek-time_frontend/src/api/config.js
+++ geek-time_frontend/src/api/config.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // 这里不使用 fetch 因为存在兼容性问题
 // 推荐使用 axios 兼容性更好
 
-export const baseUrl = "http://localhost/data"; // 全局的后端 api 前缀
+export const baseUrl = "http://47.106.230.20:8080/data"; // 全局的后端 api 前缀
 
 const axiosInstance = axios.create({
     baseURL:baseUrl
